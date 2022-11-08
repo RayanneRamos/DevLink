970b34c74ca5cb787ba84c2afda4e3b2aeabdce9
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-11-08 16:18:38 -0300

    Adicionando mais links na home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,27 @@
+import './styles.css';
+
 export function Home() {
   return (
-    <h1>Page Home</h1>
+    <div className='home-container'>
+      <h1>Rayanne Ramos</h1>
+      <span>Veja meus links 👇</span>
+      <main className='links'>
+        <section className='link-area'>
+          <a href='#'>
+            <p className='link-text'>Github</p>
+          </a>
+        </section>
+        <section className='link-area'>
+          <a href='#'>
+            <p className='link-text'>Twitter</p>
+          </a>
+        </section>
+        <section className='link-area'>
+          <a href='#'>
+            <p className='link-text'>Instagram</p>
+          </a>
+        </section>
+      </main>
+    </div>
   );
 }
