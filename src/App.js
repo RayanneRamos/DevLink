a01c339dc7668af7d6228c0bfa5718d50b8a0fb3
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-11-08 15:51:28 -0300

    Criando a rota NotFound

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { Admin } from './pages/Admin';
+import { NotFound } from './pages/NotFound';
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
     path: '/admin',
     element: <Admin />
   },
+  {
+    path: '*',
+    element: <NotFound />
+  }
 ]);
 
 export { router };
