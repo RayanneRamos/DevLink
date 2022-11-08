07e90816e9fe4271c26d522b743717c42394bb9e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-11-08 16:06:46 -0300

    Criando a estilização da página NotFound

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,8 +1,14 @@
+import './styles.css';
+import { Link } from 'react-router-dom';
+import { Logo } from '../../components/Logo';
+
 export function NotFound() {
   return (
-    <>
+    <div className='error'>
+      <Logo />
       <h1>Página não encontrada!</h1>
       <p>Essa página que você está procurando não existe.</p>
-    </>
+      <Link to='/' className='link'>Voltar para a Home</Link>
+    </div>
   );
 }
