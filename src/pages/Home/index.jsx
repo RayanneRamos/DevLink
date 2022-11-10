463c029a522959133eca3f20f9abafe931d8a433
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-11-10 16:45:57 -0300

    Buscando os links cadastrados no banco de dados e exibindo na Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,62 @@
 import './styles.css';
 import { SocialMedia } from '../../components/SocialMedia';
 import { FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
+import { getDocs, collection, orderBy, query } from 'firebase/firestore';
+import { db } from '../../services/firebaseConnection';
 
 export function Home() {
+  const [ links, setLinks ] = useState([]);
+
+  useEffect(() => {
+    async function loadLinks() {
+      const linksRef = collection(db, 'links');
+      const queryRef = query(linksRef, orderBy('created', 'asc'));
+
+      await getDocs(queryRef).then((snapshot) => {
+        let lista = [];
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            name: doc.data().name,
+            url: doc.data().url,
+            bg: doc.data().bg,
+            color: doc.data().color,
+          });
+        });
+
+        setLinks(lista);
+      });
+    }
+
+    loadLinks();
+  },[]);
+
+
   return (
     <div className='home-container'>
       <h1>Rayanne Ramos</h1>
       <span>Veja meus links 👇</span>
       <main className='links'>
-        <section className='link-area'>
-          <a href='/'>
-            <p className='link-text'>Github</p>
-          </a>
-        </section>
-        <section className='link-area'>
-          <a href='/'>
-            <p className='link-text'>Twitter</p>
-          </a>
-        </section>
-        <section className='link-area'>
-          <a href='/'>
-            <p className='link-text'>Instagram</p>
-          </a>
-        </section>
+        { links.map((item) => {
+          return (
+            <section 
+              className='link-area' 
+              key={item.id} 
+              style={{ backgroundColor: item.bg }}
+            >
+              <a href={item.url} target='blank'>
+                <p 
+                  className='link-text'
+                  style={{ color: item.color }}
+                >
+                  {item.name}
+                </p>
+              </a>
+            </section>
+          )
+        }) }
         <footer>
           <SocialMedia url='https://github.com/RayanneRamos'>
             <FaGithub size={35} color='#fff' />
